e4a33a5a2dbe155c11b7daac81833027197b4e87
VadimSimonov <dark-vadim@mail.ru>
2018-01-25 18:03:43 +0300

    restaurant create model begin

---
+++ src/main/java/restaurant/model/Meals.java
@@ -0,0 +1,10 @@
+package restaurant.model;
+
+import java.time.LocalDateTime;
+
+public class Meals {
+    private String meal;
+    private LocalDateTime dateTime;
+    private String description;
+    private int price;
+}
---
+++ src/main/java/restaurant/model/Users.java
@@ -0,0 +1,12 @@
+package restaurant.model;
+
+import java.util.Date;
+
+public class Users {
+
+    private String name;
+    private Date registered;
+    private String password;
+    private boolean enabled = true;
+    private String email;
+}
